e039c2af4e69ce3f306b54bfd61720e474f91a39
paikwiki <paikwiki@gmail.com>
2024-06-09 21:58:38 +0900

    :necktie: Book 조회나 삭제시 null 반환가능하도록 변경

--- src/books/domain/book.repository.ts
+++ src/books/domain/book.repository.ts
@@ -3,7 +3,7 @@ import { Book } from "./book.entity";
 export interface BookRepository {
   newId: () => Promise<number>;
   save(book: Book): Promise<Book>;
-  findById(id: number): Promise<Book>;
+  findById(id: number): Promise<Book | null>;
   findAll(): Promise<Book[]>;
-  remove(id: number): Promise<void>;
+  remove(id: number): Promise<number | null>;
 }
